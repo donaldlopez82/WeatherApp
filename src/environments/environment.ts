7e19f81db117aec3f824edf7bc53a65a306ab17a
donald82 <mujuro8dk@gmail.com>
2023-02-15 08:44:38 -0800

    Finished-API-Styles

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  producion: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/Perris',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '6492aca4c8mshe5b79cb6e4a394bp126c88jsna9386e572b69',
+};
